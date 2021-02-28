4a0b6514d7b8f53c4397ad1c776e0d4726c3ee2d
sekisuri - GitHub <sekisuri@gmail.com>
2021-03-01 02:02:35 +0900

    6장 페이지 이동

--- lib/main.dart
+++ lib/main.dart
@@ -5,36 +5,57 @@ void main() {
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '플러터 연습',
+      title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
-
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeScreen(),
+      home: FirstPage(),   // 첫 페이지를 시작 페이지로 지정
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+// 첫 페이지
+class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    print('FirstPage build()');
     return Scaffold(
       appBar: AppBar(
-        title: Text('Center'),
-     
+        title: Text('First'),
       ),
-      body: Center(
-        child: Container(
-          color: Colors.red,
-          width: 100,
-          height: 100,
-        ),
+      body: RaisedButton(
+        child: Text('다음 페이지로'),
+        onPressed: () async {
+          final result = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SecondPage()),
+          );
+
+          print(result);
+        },
+      ),
+    );
+  }
+}
+
+
+// 두번째 페이지
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print('SecondPage build()');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second'),
+      ),
+      body: RaisedButton(
+        child: Text('이전 페이지로'),
+        onPressed: () {
+          Navigator.pop(context, 'ok');
+        },
       ),
     );
   }
