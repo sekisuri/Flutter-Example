4d89c9e31f9e8803b92c86f316574c3d9374b385
sekisuri - GitHub <sekisuri@gmail.com>
2021-02-28 17:18:08 +0900

    4.2.4 Stack

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -29,10 +28,8 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title:  Text('제목'),
       ),
-      body: Row(
-        mainAxisSize: MainAxisSize.max, // 가로로 꽉 채우기
-        mainAxisAlignment: MainAxisAlignment.center, // 가로 방향으로 가운데 정렬하기
-        crossAxisAlignment: CrossAxisAlignment.center, // 세로 방향으로 가운데 정렬
+      body: Stack(
+
         children: <Widget>[
           Container(
             color: Colors.red,
@@ -43,15 +40,15 @@ class HomeScreen extends StatelessWidget {
           ),
           Container(
             color: Colors.green,
-            width: 100,
-            height: 100,
+            width: 80,
+            height: 80,
             padding: const EdgeInsets.all(8.0),
             margin: const EdgeInsets.all(8.0),
           ),
           Container(
             color: Colors.blue,
-            width: 100,
-            height: 100,
+            width: 60,
+            height: 60,
             padding: const EdgeInsets.all(8.0),
             margin: const EdgeInsets.all(8.0),
           ),
