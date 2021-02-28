8e84ca76bc40bdcdafe67ce8699171e6088563ce
sekisuri - GitHub <sekisuri@gmail.com>
2021-02-28 17:28:39 +0900

    4.2.6 ListView

--- lib/main.dart
+++ lib/main.dart
@@ -28,33 +28,29 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title:  Text('제목'),
       ),
-      body: Stack(
-
+      body: ListView(
+        scrollDirection: Axis.vertical,
         children: <Widget>[
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-            padding: const EdgeInsets.all(8.0),
-            margin: const EdgeInsets.all(8.0),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            trailing: Icon(Icons.navigate_next),
+            onTap: (){},
           ),
-          Container(
-            color: Colors.green,
-            width: 80,
-            height: 80,
-            padding: const EdgeInsets.all(8.0),
-            margin: const EdgeInsets.all(8.0),
+          ListTile(
+            leading: Icon(Icons.event),
+            title: Text('Event'),
+            trailing: Icon(Icons.navigate_next),
+            onTap: (){},
           ),
-          Container(
-            color: Colors.blue,
-            width: 60,
-            height: 60,
-            padding: const EdgeInsets.all(8.0),
-            margin: const EdgeInsets.all(8.0),
+          ListTile(
+            leading: Icon(Icons.camera),
+            title: Text('Camera'),
+            trailing: Icon(Icons.navigate_next),
+            onTap: (){},
           ),
         ],
-      )
+      ),
     );
   }
 }
-
