8fbb568ec16bf5253f1d59d11ade91ade424c9d2
sekisuri <sekisuri@gmail.com>
2021-02-28 15:49:07 +0900

    4.2.2 Column

--- lib/main.dart
+++ lib/main.dart
@@ -29,13 +29,31 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title:  Text('제목'),
       ),
-      body: Container(
-        color: Colors.red,
-        width: 100,
-        height: 100,
-        padding: const EdgeInsets.all(8.0),
-        margin: const EdgeInsets.all(8.0),
-      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            color: Colors.red,
+            width: 100,
+            height: 100,
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0),
+          ),
+          Container(
+            color: Colors.green,
+            width: 100,
+            height: 100,
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0),
+          ),
+          Container(
+            color: Colors.blue,
+            width: 100,
+            height: 100,
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0),
+          ),
+        ],
+      )
     );
   }
 }
