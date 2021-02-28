a1bd25124a6ccd68c21e27ba8885b80f122f0031
sekisuri - GitHub <sekisuri@gmail.com>
2021-03-01 00:57:43 +0900

    4.2.10 BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -24,26 +24,21 @@ class MyApp extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Tab'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(icon: Icon(Icons.tag_faces)),
-              Tab(text: '메뉴2'),
-              Tab(icon: Icon(Icons.info), text: '메뉴3'),
-            ],
-          ),
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          title: Text('Home'),
         ),
-        body: TabBarView(
-            children: <Widget>[
-              Container(color: Colors.yellow,),
-              Container(color: Colors.orange,),
-              Container(color: Colors.red,),
-            ]),
-      ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          title: Text('Profile'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.notifications),
+          title: Text('Notificatio'),
+        ),
+      ]),
     );
   }
 }
